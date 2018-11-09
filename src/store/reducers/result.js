0bbb92426a8ac48ca4ab7572eb0e4660e86ea223
BuffaloRob <peckham.rob@gmail.com>
2018-11-09 13:59:38 -0500

    + result.js

---
+++ src/store/reducers/result.js
@@ -0,0 +1,32 @@
+import * as actionTypes from "../actions";
+
+const initialState = {
+  results: []
+};
+
+const resultReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        // Important to use concat() instead of push(), push will manipulate original array
+        results: state.results.concat({ id: new Date(), value: action.result })
+      };
+    case actionTypes.DELETE_RESULT:
+      // Can be done this way also
+      // const id =  2;
+      // const newArray = [...state.results];
+      // newArray.splice(id, 1)
+
+      const newArray = state.results.filter(
+        result => result.id !== action.resultElId
+      );
+      return {
+        ...state,
+        results: newArray
+      };
+  }
+  return state;
+};
+
+export default resultReducer;
