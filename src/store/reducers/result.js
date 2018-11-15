ec41c3eaed2caa5d8ae134daa45f82af88f9ce88
BuffaloRob <peckham.rob@gmail.com>
2018-11-15 10:46:38 -0500

    result: abstracted deleteResult out of case statement to make leaner

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,18 +5,16 @@ const initialState = {
   results: []
 };
 
+const deleteResult = (state, action) => {
+  const newArray = state.results.filter(result => result.id !== action.resultElId);
+  return updateObject(state, { results: newArray });
+}
+
 const resultReducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.STORE_RESULT:
-       // Important to use concat() instead of push(), push will manipulate original array
-      return updateObject(state, {results: state.results.concat({ id: new Date(), value: action.result})});
-    case actionTypes.DELETE_RESULT:
-      // Can be done this way also
-      // const id =  2;
-      // const newArray = [...state.results];
-      // newArray.splice(id, 1)
-      const newArray = state.results.filter(result => result.id !== action.resultElId);
-      return updateObject(state, {results: newArray});
+    // Important to use concat() instead of push(), push will manipulate original array
+    case actionTypes.STORE_RESULT: return updateObject(state, {results: state.results.concat({ id: new Date(), value: action.result})});
+    case actionTypes.DELETE_RESULT: return deleteResult(state, action);
   }
   return state;
 };
