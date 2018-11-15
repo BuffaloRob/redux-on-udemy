9c7d6ecce5e07faedfd1eb18fd9363aa94725998
BuffaloRob <peckham.rob@gmail.com>
2018-11-15 10:38:11 -0500

    +using utility func to decrease size of switch statements

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
+import { updateObject } from '../utility';
 
 const initialState = {
   results: []
@@ -7,24 +8,15 @@ const initialState = {
 const resultReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        // Important to use concat() instead of push(), push will manipulate original array
-        results: state.results.concat({ id: new Date(), value: action.result })
-      };
+       // Important to use concat() instead of push(), push will manipulate original array
+      return updateObject(state, {results: state.results.concat({ id: new Date(), value: action.result})});
     case actionTypes.DELETE_RESULT:
       // Can be done this way also
       // const id =  2;
       // const newArray = [...state.results];
       // newArray.splice(id, 1)
-
-      const newArray = state.results.filter(
-        result => result.id !== action.resultElId
-      );
-      return {
-        ...state,
-        results: newArray
-      };
+      const newArray = state.results.filter(result => result.id !== action.resultElId);
+      return updateObject(state, {results: newArray});
   }
   return state;
 };
